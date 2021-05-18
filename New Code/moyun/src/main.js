e1e641ef03b5732abeb90e7dcfc8ce894bac6e58
Yang-Zhao <823504052@qq.com>
2021-05-18 20:46:29 +0800

    首页

--- New Code/moyun/src/main.js
+++ New Code/moyun/src/main.js
@@ -3,11 +3,17 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import ElementUI from "element-ui";
+import "./styles.scss";
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
--- New Code/moyun/src/views/Home.vue
+++ New Code/moyun/src/views/Home.vue
@@ -1,18 +1,311 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <bar></bar>
+    <v-img src="../assets/widthPic.jpg" dark></v-img>
+    <h1 class="textCenter">墨韵</h1>
+    
+
+    <v-main>
+      <v-container fluid>
+        <h2 class="text-center">
+          热门圈子
+          <v-container class="pa-4 text-center">
+            <v-row class="fill-height" align="center" justify="center">
+              <template v-for="(group, i) in groups">
+                <v-col :key="i" cols="12" md="2" >
+                  <v-hover v-slot="{ hover }">
+                    <v-card
+                      :elevation="hover ? 24 : 2"
+                      :class="{ 'on-hover': hover }"
+                      width=300
+                    >
+                      <v-img :src="group.img" height="225px">
+                        <v-card-title class="title white--text">
+                          <v-row
+                            class="fill-height flex-column"
+                            justify="space-between"
+                          >
+                            <p class="mt-4 subheading text-left">
+                              {{ group.title }}
+                            </p>
+
+                            <div>
+                              <p
+                                class="
+                                  ma-0
+                                  body-1
+                                  font-weight-bold font-italic
+                                  text-left
+                                "
+                              >
+                                {{ group.text }}
+                              </p>
+                              <p
+                                class="
+                                  caption
+                                  font-weight-medium font-italic
+                                  text-left
+                                "
+                              >
+                                {{ group.subtext }}
+                              </p>
+                            </div>
+
+                          </v-row>
+                        </v-card-title>
+                      </v-img>
+                    </v-card>
+                  </v-hover>
+                </v-col>
+              </template>
+            </v-row>
+          </v-container>
+        </h2>
+      </v-container>
+      <v-container fluid>
+        <h2 class="text-center">
+          热门书籍
+          <v-container class="pa-4 text-center">
+            <v-row class="fill-height" align="center" justify="center">
+              <template v-for="(group, i) in groups">
+                <v-col :key="i" cols="12" md="2" >
+                  <v-hover v-slot="{ hover }">
+                    <v-card
+                      :elevation="hover ? 24 : 2"
+                      :class="{ 'on-hover': hover }"
+                      width=300
+                    >
+                      <v-img :src="group.img" height="225px">
+                        <v-card-title class="title white--text">
+                          <v-row
+                            class="fill-height flex-column"
+                            justify="space-between"
+                          >
+                            <p class="mt-4 subheading text-left">
+                              {{ group.title }}
+                            </p>
+
+                            <div>
+                              <p
+                                class="
+                                  ma-0
+                                  body-1
+                                  font-weight-bold font-italic
+                                  text-left
+                                "
+                              >
+                                {{ group.text }}
+                              </p>
+                              <p
+                                class="
+                                  caption
+                                  font-weight-medium font-italic
+                                  text-left
+                                "
+                              >
+                                {{ group.subtext }}
+                              </p>
+                            </div>
+
+                          </v-row>
+                        </v-card-title>
+                      </v-img>
+                    </v-card>
+                  </v-hover>
+                </v-col>
+              </template>
+            </v-row>
+          </v-container>
+        </h2>
+      </v-container>
+    </v-main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import bar from "../components/Bar.vue";
 export default {
-  name: "Home",
+  
+  data: () => ({
+    groups: [
+      {
+        title: "New Releases",
+        text: `It's New Release Friday`,
+        subtext: "Newly released songs. Updated daily.",
+        img: "https://images.unsplash.com/photo-1429514513361-8fa32282fd5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3264&q=80",
+      },
+      {
+        title: "Rock",
+        text: "Greatest Rock Hits",
+        subtext: "Lose yourself in rock tunes.",
+        img: "https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+    ],
+    books: [
+      {
+        title: "New Releases",
+        text: `It's New Release Friday`,
+        subtext: "Newly released songs. Updated daily.",
+        img: "https://images.unsplash.com/photo-1429514513361-8fa32282fd5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3264&q=80",
+      },
+      {
+        title: "Rock",
+        text: "Greatest Rock Hits",
+        subtext: "Lose yourself in rock tunes.",
+        img: "https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+      {
+        title: "Mellow Moods",
+        text: "Ambient Bass",
+        subtext: "Chill beats to mellow you out.",
+        img: "https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80",
+      },
+    ],
+    transparent: "rgba(255, 255, 255, 0)",
+  }),
   components: {
-    HelloWorld,
+    bar,
   },
 };
 </script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+}
+.home v-img {
+  height: 100%;
+  width: 100%;
+  position: abosulte;
+  background-size: cover;
+  opacity: 0.8;
+  filter: alpha(opacity=60);
+}
+.textCenter{
+	position:absolute;
+  text-align:center;
+  left:50%;
+	top:30%;
+  margin-left: -200px;
+	text-shadow: 5px -5px black, 4px -4px white;
+  font-weight: bold;
+  -webkit-text-fill-color: transparent;
+  font-size:200px
+}
+
+</style>
