7b07e95ff78bdda2f26475f0354ef15f0a5f3c6f
Yang-Zhao <823504052@qq.com>
2021-06-08 12:05:33 +0800

    不知道什么时候的改动

--- New Code/moyun/src/views/ForgetPassword.vue
+++ New Code/moyun/src/views/ForgetPassword.vue
@@ -6,6 +6,8 @@
           <v-form ref="form" v-model="valid" lazy-validation>
             <img src="../assets/logo.png" id="logo" /><br />
             <v-cintainer>
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
               <v-text-field
                 v-model="id"
                 :rules="idRules"
@@ -19,6 +21,44 @@
                 label="E-mail"
                 required
               ></v-text-field>
+              <v-row>
+                <v-col cols="12" sm="8">
+              <v-text-field
+                v-model="code"
+                :rules="codeRules"
+                label="验证码"
+                required
+              ></v-text-field>
+              <template v-slot:append-outer>
+                  <v-btn
+                    text
+                    style="top: -12px"
+                    offset-y
+                  > 
+                    获取验证码
+                  </v-btn>
+              </template>
+                </v-col>
+              </v-row>
+              <v-text-field
+                v-model="password"
+                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[passwordRules]"
+                :type="show2 ? 'text' : 'password'"
+                label="密码"
+                required
+                @click:append="show2 = !show2"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="rePassword"
+                :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show3 ? 'text' : 'password'"
+                :rules="[passwordRules,affirmPass]"
+                label="确认密码"
+                required
+                @click:append="show3 = !show3"
+              ></v-text-field>
 
               <v-btn
                 :disabled="!valid"
@@ -27,8 +67,10 @@
                 large
                 href="/Login"
               >
-                <p class="login_">查询</p>
+                <p class="pass_">重置密码</p>
               </v-btn>
+                </v-col>
+                  </v-row>
             </v-cintainer>
           </v-form>
         </v-container>
@@ -40,14 +82,21 @@
 <script>
 export default {
   data: () => ({
+    show2:false,
+    show3:false,
     valid: true,
     id: "",
-    idRules: [(v) => !!v || "ID is required"],
+    idRules: [(v) => !!v || "请填写账号"],
+    password: "",
+    passwordRules: (v) => !!v || "请填写密码",
+    rePassword:"",
     Email: "",
     emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      (v) => !!v || "请填写邮箱",
+      (v) => /.+@.+\..+/.test(v) || "邮箱格式不合法",
     ],
+    code:"",
+    codeRules:[(v) => !!v || "请填写验证码"],
     checkbox: false,
   }),
 
@@ -55,11 +104,11 @@ export default {
     validate() {
       this.$refs.form.validate();
     },
-    reset() {
-      this.$refs.form.reset();
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation();
+    affirmPass(val) {
+      if (val !== this.password) {
+        return "两次密码不一致";
+      }
+      return true;
     },
   },
 };
@@ -87,15 +136,15 @@ export default {
   transition: box-shadow 0.2s ease-out;
   background-color: #efeeee;
   position: relative;
-  top: 100px;
-  margin-top: 20px;
+  top: 5%;
+  margin-top: 10px;
   width: 130px;
   height: 55px;
   outline: none;
   border: none;
 }
-.login_ {
-  font-size: 20px;
+.pass_ {
+  font-size: 18px;
   margin-top: 15px;
 }
 .button:hover {
@@ -114,7 +163,7 @@ export default {
   transform: translate(-50%, -50%);
   height: 700px;
   width: 550px;
-  z-index: 1;
+
   transition: transform 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
 .card .front {
@@ -130,7 +179,5 @@ export default {
   flex-direction: column;
   transition: 1s cubic-bezier(0.68, -0.55, 0.265, 1.55);
 }
-.card .front {
-  z-index: 1;
-}
+
 </style>
--- New Code/moyun/src/views/Login.vue
+++ New Code/moyun/src/views/Login.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="login">
     <div class="card">
-      <div class="front">
+      <div class="front" :class="isTop  ? 'contain-Before' : ''" id="1">
         <v-container>
-          <v-form ref="form" v-model="valid" lazy-validation>
+          <v-form ref="form" lazy-validation>
             <img src="../assets/logo.png" id="logo" /><br />
             <v-container>
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
               <v-text-field
                 v-model="id"
                 :rules="idRules"
@@ -14,10 +16,13 @@
               ></v-text-field>
 
               <v-text-field
-                v-model="password"
+                v-model="pass"
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
                 :rules="passwordRules"
+                :type="show1 ? 'text' : 'password'"
                 label="密码"
                 required
+                @click:append="show1 = !show1"
               ></v-text-field>
 
               <v-btn
@@ -29,16 +34,75 @@
               >
                 <p class="login_">登录</p>
               </v-btn>
+                </v-col>
+              </v-row>
             </v-container>
           </v-form>
         </v-container>
         <v-container>
           <v-col justify="center" cols="12">
-            <v-btn text color="primary" class="register" href="/Register"
+            <v-btn text color="primary" class="register" @click="toRegister"
               >注册</v-btn
             >
             <v-btn text color="primary" class="forget" href="/ForgetPassword">忘记密码</v-btn>
           </v-col>
+        </v-container>
+      </div>
+      <div class="back" :class="isTop  ? 'contain-After' : ''" id="2">
+        <v-container>
+          <v-form ref="form"  lazy-validation>
+            <img src="../assets/logo.png" id="logo" /><br />
+            <v-container>
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
+              <v-text-field
+                v-model="id"
+                :rules="idRules"
+                label="学号/教工号"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="password"
+                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[passwordRules]"
+                :type="show2 ? 'text' : 'password'"
+                label="密码"
+                required
+                @click:append="show2 = !show2"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="rePassword"
+                :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show3 ? 'text' : 'password'"
+                :rules="[passwordRules,affirmPass]"
+                label="确认密码"
+                required
+                @click:append="show3 = !show3"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="Email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+              ></v-text-field>
+
+              <v-btn :disabled="!valid" class="button" large href="/Login">
+                <p class="register_">注册</p>
+              </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-container>
+        <v-container>
+            <v-row>
+          <v-col justify="center" cols="12" >
+            <v-btn text color="primary" class="return" @click="toLogin" >登录</v-btn>
+          </v-col>
+          </v-row>
         </v-container>
       </div>
     </div>
@@ -46,27 +110,54 @@
 </template>
 
 <script>
-export default {
-  data: () => ({
-    valid: true,
-    id: "",
-    idRules: [(v) => !!v || "ID is required"],
-    password: "",
-    passwordRules: [(v) => !!v || "Password is required"],
-    checkbox: false,
-  }),
 
-  methods: {
-    validate() {
+export default {
+  
+  data() {
+    
+    return {
+    valid: true,
+    show1: false,
+    show2: false,
+    show3: false,
+    isTop:false,
+    id: "",
+    idRules: [(v) => !!v || "请填写账号"],
+    pass:"",
+    password: "",
+    passwordRules: (v) => !!v || "请填写密码",
+    rePassword:"",
+    Email:"",
+    emailRules:[
+        v => !!v || '请填写邮箱',
+        v => /.+@.+\..+/.test(v) || '邮箱格式不合法',
+    ],
+    checkbox: false,
+    };
+
+  },
+methods: {
+  toRegister(){
+    if(!this.isTop){
+         this.isTop = true
+     }
+  },
+  toLogin(){
+    if(this.isTop){
+         this.isTop = false
+     }
+  },
+  
+  validate() {
       this.$refs.form.validate();
     },
-    reset() {
-      this.$refs.form.reset();
+  affirmPass(val) {
+      if (val !== this.password) {
+        return "两次密码不一致";
+      }
+      return true;
     },
-    resetValidation() {
-      this.$refs.form.resetValidation();
-    },
-  },
+},
 };
 </script>
 
@@ -92,7 +183,7 @@ export default {
   transition: box-shadow 0.2s ease-out;
   background-color: #efeeee;
   position: relative;
-  top: 100px;
+  top: 15%;
   margin-top: 20px;
   width: 130px;
   height: 55px;
@@ -111,6 +202,10 @@ export default {
   font-size: 20px;
   margin-top: 15px;
 }
+.register_ {
+  font-size: 20px;
+  margin-top: 15px;
+}
 .card {
   position: absolute;
   left: 50%;
@@ -118,10 +213,10 @@ export default {
   transform: translate(-50%, -50%);
   height: 700px;
   width: 550px;
-  z-index: 1;
+
   transition: transform 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
-.card .front {
+.card .front,.card .back {
   position: absolute;
   text-align: center;
   box-shadow: 12px 12px 24px rgba(0, 0, 0, 0.1),
@@ -134,13 +229,34 @@ export default {
   flex-direction: column;
   transition: 1s cubic-bezier(0.68, -0.55, 0.265, 1.55);
 }
-.card .front {
-  z-index: 1;
+.front,.back {
+  position: absolute;
+  transition: 0.3s linear;
+  backface-visibility:hidden;
 }
 .register {
   top: 130px;
 }
 .forget {
   top: 130px;
+}
+.turn{
+    text-align: right;
+}
+
+.back{
+    transform: rotateY(-180deg);
+}
+.front {
+  transform: rotateY(0deg);
+}
+.return{
+    top:40px;
+}
+.card .contain-Before{
+    transform: rotateY(-180deg);
+}
+.card .contain-After{
+    transform: rotateY(0deg);
 }
 </style>
