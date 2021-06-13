e9fafca3ad0d689af2a7f28f8643f204a2dd52ae
Yang-Zhao <823504052@qq.com>
2021-06-14 02:05:11 +0800

    继续修改跨域问题

--- New Code/moyun/vue.config.js
+++ New Code/moyun/vue.config.js
@@ -6,16 +6,6 @@ module.exports = {
             warning: false,
             errors: false
         },
-        proxy: {
-            '/api': {
-                target: 'http://39.105.38.175:8080/', //别忘记加协议！！如http://
-                secure: false, // 如果是 https ,需要开启这个选项
-                changeOrigin: true, //开启跨域
-                pathRewrite: {
-                    '^/api': '/' //正则匹配，相当于用api替换target中的地址
-                }
-            }
-        }
     },
     publicPath: './',
     outputDir: 'dist'
