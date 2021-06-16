c79a39d88a5d505b49476fa6b00518c92e3f44ec
Yang-Zhao <823504052@qq.com>
2021-06-16 19:39:01 +0800

    更改过渡动画

--- New Code/moyun/src/App.vue
+++ New Code/moyun/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <router-view v-if="isRouterAlive"></router-view>
+        <router-view v-if="isRouterAlive"></router-view>
     </v-main>
   </v-app>
 </template>
--- New Code/moyun/src/main.js
+++ New Code/moyun/src/main.js
@@ -8,6 +8,8 @@ import ElementUI from "element-ui";
 import "./styles.scss";
 import axios from "axios"
 import qs from "qs";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.use(ElementUI);
 
@@ -16,6 +18,26 @@ axios.defaults.baseURL ='/api/'
 Vue.prototype.$http = axios
 Vue.prototype.$qs = qs
 Vue.config.devtools = true;
+
+NProgress.configure({     
+       easing: 'ease',  // 动画方式    
+       speed: 500,  // 递增进度条的速度    
+       showSpinner: false, // 是否显示加载ico    
+       trickleSpeed: 200, // 自动递增间隔    
+       minimum: 0.3 // 初始化时的最小百分比
+  })
+//当路由进入前
+router.beforeEach((to, from , next) => {
+  // 每次切换页面时，调用进度条
+ NProgress.start();
+ // 这个一定要加，没有next()页面不会跳转的。这部分还不清楚的去翻一下官网就明白了
+  next();
+});
+//当路由进入后：关闭进度条
+router.afterEach(() => {  
+ // 在即将进入新的页面组件前，关闭掉进度条
+ NProgress.done()
+})
 
 new Vue({
   router,
